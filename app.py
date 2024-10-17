a7f366d4f1b3d7ad20d5f7892add6b1dad3306e3
luantaraschi <luantaraschi@gmail.com>
2024-10-17 11:57:31 -0300

    sistema com "match" aderido

--- app.py
+++ app.py
@@ -1,31 +1,48 @@
 import os
 
-
-print('''
+def exibir_nome():
+      print('''
 █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
 ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
       ''')
 
-print('1. Cadastrar restauras')
-print('2. Listar restaurantes')
-print('3. Ativar restaurantes')
-print('4. Sair\n')
-
-opcao_escolhida = int(input('Escolha uma opcao: '))
-# opcao_escolhida = int(opcao_escolhida)
+def exibir_opcoes():
+      print('1. Cadastrar restauras')
+      print('2. Listar restaurantes')
+      print('3. Ativar restaurantes')
+      print('4. Sair\n')
 
 def finalizar_app():
       os.system('cls')
       print('Encerrando!\n')
 
-if opcao_escolhida == 1:
-      print('Cadastrar restaurantes')
-elif opcao_escolhida == 2:
-      print('Listar restaurantes')
-elif opcao_escolhida == 3:
-      print('Ativar restaurante')
-else:
-      finalizar_app()
+def opcao_invalida():
+      print('Opcao invalida!\n')
+      input('Pressione enter para voltar ao menu')
+      main()
 
+def escolher_opcao():
+      try:
+            opcao_escolhida = int(input('Escolha uma opcao: '))
+            match opcao_escolhida:
+                  case 1:
+                        print('Adicionar restaurante')
+                  case 2:
+                        print('Listar restaurantes')
+                  case 3:
+                        print('Ativar restaurante')
+                  case 4:
+                        finalizar_app()
+                  case _:
+                        opcao_invalida()
+      except:
+            opcao_invalida()
 
-    
+def main():
+      os.system('cls')
+      exibir_nome()
+      exibir_opcoes()
+      escolher_opcao()
+
+if __name__ == '__main__':
+      main()
