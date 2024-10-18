02ba70110b62a1ba4e5060ac9efde029eb741074
luantaraschi <luantaraschi@gmail.com>
2024-10-18 10:16:09 -0300

    adicionadas funcoes de adicionar e listar restaurantes

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 
+restaurantes = ['Tropicália', 'Baião Bistro', 'Yaki Station']
+
 def exibir_nome():
       print('''
-█▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ ▀▄▀ █▀█ █▀█ █▀▀ █▀ █▀
-▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ █░█ █▀▀ █▀▄ ██▄ ▄█ ▄█
+█▀█ ▄▀█ █▀█ █ █▀▄ █ █▀ █░█
+█▀▄ █▀█ █▀▀ █ █▄▀ █ ▄█ █▀█
       ''')
 
 def exibir_opcoes():
@@ -21,14 +23,31 @@ def opcao_invalida():
       input('Pressione enter para voltar ao menu')
       main()
 
+def cadastrar_restaurante():
+      os.system('cls')
+      print('Cadastro de novos restaurantes\n')
+      nome_do_restaurante = input('Digite o nome do restautante a ser cadastrado: ')
+      restaurantes.append(nome_do_restaurante)
+      print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
+      input('\nPressione enter para voltar ao menu')
+      main()
+
+def listar_restaurantes():
+      os.system('cls')
+      print('Lista dos restaurantes cadastrados\n')
+      for restaurante in restaurantes:
+            print(f'.{restaurante}')
+      input('\nPressione enter para voltar ao menu')
+      main()
+
 def escolher_opcao():
       try:
             opcao_escolhida = int(input('Escolha uma opcao: '))
             match opcao_escolhida:
                   case 1:
-                        print('Adicionar restaurante')
+                        cadastrar_restaurante()
                   case 2:
-                        print('Listar restaurantes')
+                        listar_restaurantes()
                   case 3:
                         print('Ativar restaurante')
                   case 4:
